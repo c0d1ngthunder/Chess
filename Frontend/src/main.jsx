9ace3b71db84c482f7bd8b63416a27474394db10
Nitish Kumar <codingthunder3@gmail.com>
2025-04-18 18:03:58 +0530

    Fix: Corrected import case for Context.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import "./index.css";
 import App from "./App.jsx";
-import Context from "./context/context.jsx";
+import Context from "./context/Context";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
